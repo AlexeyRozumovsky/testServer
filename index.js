9b3672689739a1e216feb10e1f3c4d51f75d9fe6
Vladimir Liannoy <Vladimir.Liannoy@netent.com>
2016-12-04 12:31:27 +0200

    post requests

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/test"] = requestHandlers.test;
+handle["/get/people"] = requestHandlers.getPeople;
+handle["/update/people"] = requestHandlers.updatePeople;
+handle["/remove/people"] = requestHandlers.removePeople;
 
 
 server.startServer(router.route, handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var querystring = require('querystring');
 var people = require("./people_without_smiles");
 
@@ -9,25 +10,129 @@ function start(request, response) {
     response.end(JSON.stringify(people));
 }
 
+function test(request, response) {
+    var queryData = "";
+
+    request.on('data', function (data) {
+        queryData += data;
+    });
+
+    request.on('end', function () {
+        var postData = querystring.parse(queryData);
+        console.log(postData);
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(postData));
+    });
+
+}
+
+function getPeople(request, response) {
+    var people = readJSON("people");
+
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(people));
+
+}
+
+function updatePeople(request, response) {
+    var people = readJSON("people"),
+        queryData = "";
+
+    request.on('data', function (data) {
+        queryData += data;
+    });
+
+    request.on('end', function () {
+        var postData = querystring.parse(queryData),
+            curPerson = {},
+            dataChanged = false;
+
+        people.forEach(function (person) {
+            if (person.id == postData.id) {
+                if (postData.name && postData.name.length > 3) {
+                    person.name = postData.name;
+                }
+                if (postData.photo && postData.photo.length > 10) {
+                    person.photo = postData.photo;
+                }
+                curPerson = person;
+                dataChanged = true;
+            }
+        });
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(curPerson));
+
+        if(dataChanged){
+            writeJSON("people", people);
+        }
+    });
+
+}
+
+function removePeople(request, response) {
+    var people = readJSON("people"),
+        queryData = "";
+
+    request.on('data', function (data) {
+        queryData += data;
+    });
+
+    request.on('end', function () {
+        var postData = querystring.parse(queryData),
+            curPerson = {},
+            dataChanged = false;
+
+        people.forEach(function (person, index) {
+            if (person.id == postData.id) {
+
+               people.splice(index, 1);
+                dataChanged = true;
+            }
+        });
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify({result: "ok"}));
+
+        if(dataChanged){
+            writeJSON("people", people);
+        }
+    });
+
+}
+
+
 function upload(request, response) {
 
-     var queryData = "";
+    var queryData = "";
 
-    request.on('data', function(data) {
+    request.on('data', function (data) {
         queryData += data;
     });
 
 
-
-
-    request.on('end', function() {
-        var obj = JSON.stringify(queryData);
-        console.log(queryData);
+    request.on('end', function () {
+        var obj = querystring.parse(queryData);
+        console.log(obj);
         // response.write(JSON.stringify(postData));
         // response.writeHead(413, {'Content-Type': 'application/json'});
         // response.end();
     });
 }
 
+
+function readJSON(path) {
+    return JSON.parse(fs.readFileSync("db/" + path + ".json", 'utf8'));
+}
+
+function writeJSON(path, content) {
+    fs.writeFile("db/" + path + ".json", JSON.stringify(content, null, 4));
+}
+
+exports.test = test;
 exports.start = start;
 exports.upload = upload;
+exports.getPeople = getPeople;
+exports.updatePeople = updatePeople;
+exports.removePeople = removePeople;
